0f5e7eb66e63bd49f65b8e2e2e551fe5af4096fe
Bruno Wagner <bruno.wagner.b.r@gmail.com>
2018-02-18 08:54:53 -0300

    Estilizando - Sobre

--- sobre.php
+++ sobre.php
@@ -1,17 +1,87 @@
 <?php
 session_start();
 include_once "header.php";
+include_once "section_header.php";
 ?>
-    <h2> Sobre </h2>
-    <h3> O que somos?</h3>
-    <p> Esse website tem como objetivo oferecer gratuitamente material didático sobre o assunto de geometria plana para alunos e professores.
-      Acreditando que todo conhecimento deve ser compartilhado gratuitamente, desenvolvemos essa plataforma para auxiliar no aprendizado da matemática. </p>
-      <h3>Como surgiu?</h3>
-      <p>A ideia surgiu a partir de um trabalho de conclusão de curso. Pensou-se em um produto que possa ser feito para o tcc e também possa auxiliar
-        a população. Aproveitando uma das 'paixões', matemática, decidimos produzir algo que envolva matemática e nossos conhecimentos sobre informática.
-        Surge aí a ideia: criar um site com, a principio, material gratuito e de qualidade sobre a geometria plana. Posteriormente o site pode ser aperfeiçoado
-        e conter mais coisas. </p>
-      <h3>Equipe: </h3>
-      <p>Rosangela Rafaela :)</p>
+
+
+<!-- Article section -->
+
+<div class="spad">
+  <div class="container box">
+    <div class="section-title dark">
+      <h2>Sobre</h2>
+    </div>
+    
+    <div class="row">
+      <!-- single service -->
+      <div class="col-md-6 col-sm-6">
+        <div class="service">
+          <div class="icon">
+            <i class="flaticon-012-cube"></i>
+          </div>
+          <div class="service-text">
+            <h2>O que somos?</h2>
+            <p>Esse website tem como objetivo oferecer gratuitamente material didático sobre o assunto de geometria plana para alunos e professores. Acreditando que todo conhecimento deve ser compartilhado gratuitamente, desenvolvemos essa plataforma para auxiliar no aprendizado da matemática.</p>
+          </div>
+        </div>
+      </div>
+      <!-- single service -->
+      <div class="col-md-6 col-sm-6">
+        <div class="service">
+          <div class="icon">
+            <i class="flaticon-036-brainstorming"></i>
+          </div>
+          <div class="service-text">
+            <h2>Como surgiu?</h2>
+            <p>A ideia surgiu a partir de um trabalho de conclusão de curso. Pensou-se em um produto que possa ser feito para o tcc e também possa auxiliar a população. Aproveitando uma das 'paixões', matemática, decidimos produzir algo que envolva matemática e nossos conhecimentos sobre informática. Surge aí a ideia: criar um site com, a principio, material gratuito e de qualidade sobre a geometria plana. Posteriormente o site pode ser aperfeiçoado e conter mais coisas.</p>
+          </div>
+        </div>
+      </div>
+      <!-- single service -->
+      
+    </div>
+
+  </div>
+</div>
+<!-- article section end -->
+
+<!-- Team Section -->
+  <div class="team-section spad">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="section-title">
+        <h2><span>EQUIPE</span></h2>
+      </div>
+      <div class="row">
+        <!-- single member -->
+        <div class="col-sm-4">
+          <div class="member">
+            <img src="images/team/1.jpg" alt="">
+            <h2>Christinne Williams</h2>
+            <h3>Project Manager</h3>
+          </div>
+        </div>
+        <!-- single member -->
+        <div class="col-sm-4">
+          <div class="member">
+            <img src="images/team/2.jpg" alt="">
+            <h2>Christinne Williams</h2>
+            <h3>Junior developer</h3>
+          </div>
+        </div>
+        <!-- single member -->
+        <div class="col-sm-4">
+          <div class="member">
+            <img src="images/team/3.jpg" alt="">
+            <h2>Christinne Williams</h2>
+            <h3>Digital designer</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Team Section end-->
+
 <?php
   include_once "footer.php";
